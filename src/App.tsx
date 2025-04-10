83a0aca8a3f5b524bb6cc9a53e7403e074bc24e0
Prabhu Saravanan <prabhu.saravanan@icloud.com>
2025-04-10 06:44:38 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="bg-red-500 text-white p-4">
+  Tailwind Working Test
+</div>
+    </>
+  )
+}
+
+export default App
