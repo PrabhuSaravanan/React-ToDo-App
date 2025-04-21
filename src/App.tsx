c73f18fdc7788131e64fbd91a684934b2cbb99d0
Prabhu Saravanan <prabhu.saravanan@icloud.com>
2025-04-21 17:09:54 +0530

    ... Listed Out Items

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-// App.tsx
-import { ToDoProvider } from "./context/ToDoContext";
 import { AddToDo } from "./components/AddToDo";
+import { ToDoList } from "./components/ToDoList";
+import { ToDoProvider } from "./context/ToDoContext";
 
 function App() {
   return (
     <ToDoProvider>
-      <div className="p-4">
-        <h1 className="text-xl font-bold mb-4">ToDo App</h1>
+      <div className="max-w-md mx-auto mt-8 p-4">
+        <h1 className="text-2xl font-bold mb-4">Todo List</h1>
         <AddToDo />
+        <div className="mt-4">
+          <ToDoList />
+        </div>
       </div>
     </ToDoProvider>
   );
